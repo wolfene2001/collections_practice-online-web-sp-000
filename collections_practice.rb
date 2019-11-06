c81f0ca1d1d5a5b5b54ce0941dad4e102103fb58
wolfene2001 <wolfene2001@yahoo.com>
2019-11-06 15:56:57 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,41 @@
+def sort_array_asc(array)
+  array = [25, 7, 1]
+  array.sort do |a,b|
+  a <=> b
+end
+end
 
+def sort_array_desc(array)
+  array = [25, 7, 14]
+  array.sort do |a,b|
+  b <=> a
+end
+end
+
+def sort_array_char_count(array)
+
+end
+
+def swap_elements(array)
+
+end
+
+def reverse_array(array)
+
+end
+
+def kesha_maker(array)
+
+end
+
+def find_a
+
+end
+
+def sum_array(array)
+
+end
+
+def add_s(array)
+
+end
