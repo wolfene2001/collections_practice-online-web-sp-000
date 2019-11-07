0b789c8798c5a4fd11d8ee1e98a2baecf50d6d58
wolfene2001 <wolfene2001@yahoo.com>
2019-11-07 22:34:23 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,10 +13,12 @@ end
 end
 
 def sort_array_char_count(array)
-
+  result = array.sort {|left, right| left.length <=> right.length}
 end
 
+
 def swap_elements(array)
+  array[0], array[1], array[2] = array[0], array[2], array[1]
 
 end
 
