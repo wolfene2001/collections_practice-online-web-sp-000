8e78fcd6d3064a431a54e766a7cbd2f094c32962
wolfene2001 <wolfene2001@yahoo.com>
2019-11-09 18:50:09 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -18,26 +18,31 @@ end
 
 
 def swap_elements(array)
-  array[0], array[1], array[2] = array[0], array[2], array[1]
+  array[1], array[2] = array[2], array[1]
+return array
+  end
 
-end
 
 def reverse_array(array)
-
+  array.reverse
 end
 
 def kesha_maker(array)
-
+  array.each do |word|
+    word[2] = "$"
+  end
 end
 
-def find_a
-
+def find_a(array)
+  array.select {|word|word.start_with?("a")}
 end
 
 def sum_array(array)
-
+  array.inject(0) {|sum, i| sum + i}
 end
 
 def add_s(array)
-
+  array.each_with_index.map do |element, index|
+    index == 1? element : element + "s"
+  end
 end
